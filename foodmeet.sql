f626496f5d6d5ff95b81a1350675f2f035fc4ce3
Seksan Sukkasem <153727319+65160410@users.noreply.github.com>
2025-02-18 23:51:13 +0700

    Add files via upload

---
+++ foodmeet.sql
@@ -0,0 +1,300 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 18, 2025 at 05:50 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `foodmeet`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menuitems`
+--
+
+CREATE TABLE `menuitems` (
+  `ItemID` int(11) NOT NULL,
+  `RestaurantID` int(11) DEFAULT NULL,
+  `Description` text DEFAULT NULL,
+  `Price` decimal(10,2) DEFAULT NULL,
+  `ItemName` varchar(255) NOT NULL,
+  `ImageURL` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menupreferences`
+--
+
+CREATE TABLE `menupreferences` (
+  `UserID` int(11) NOT NULL,
+  `ItemID` int(11) NOT NULL,
+  `PreferenceType` tinyint(4) NOT NULL CHECK (`PreferenceType` in (-1,0,1))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `restaurants`
+--
+
+CREATE TABLE `restaurants` (
+  `RestaurantID` int(11) NOT NULL,
+  `Description` text DEFAULT NULL,
+  `Location` varchar(255) DEFAULT NULL,
+  `AverageRating` decimal(4,2) DEFAULT NULL,
+  `PopularityScore` int(11) DEFAULT NULL,
+  `RestaurantName` varchar(255) NOT NULL,
+  `Address` varchar(255) DEFAULT NULL,
+  `Phone` varchar(20) DEFAULT NULL,
+  `Website` varchar(255) DEFAULT NULL,
+  `CuisineType` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviews`
+--
+
+CREATE TABLE `reviews` (
+  `ReviewID` int(11) NOT NULL,
+  `UserID` int(11) DEFAULT NULL,
+  `RestaurantID` int(11) DEFAULT NULL,
+  `Rating` int(11) DEFAULT NULL CHECK (`Rating` between 1 and 5),
+  `Comment` text DEFAULT NULL,
+  `ReviewTime` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roommembers`
+--
+
+CREATE TABLE `roommembers` (
+  `RoomID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `JoinedAt` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rooms`
+--
+
+CREATE TABLE `rooms` (
+  `RoomID` int(11) NOT NULL,
+  `CreatedBy` int(11) DEFAULT NULL,
+  `CreatedAt` timestamp NOT NULL DEFAULT current_timestamp(),
+  `RoomCode` varchar(20) NOT NULL,
+  `room_name` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `rooms`
+--
+
+INSERT INTO `rooms` (`RoomID`, `CreatedBy`, `CreatedAt`, `RoomCode`, `room_name`, `description`) VALUES
+(6, 13, '2025-02-18 15:09:33', '5IUPDQ', 'sek123', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `UserID` int(11) NOT NULL,
+  `Preferences` text DEFAULT NULL,
+  `DietaryRestrictions` text DEFAULT NULL,
+  `Username` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `Email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`UserID`, `Preferences`, `DietaryRestrictions`, `Username`, `Password`, `Email`) VALUES
+(13, NULL, NULL, 'Seksant Sukkasem', '1234', 'admin1234@gmail.com'),
+(21, NULL, NULL, 'Seksant', '123', 'admin@go.buu.ac.th');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `votes`
+--
+
+CREATE TABLE `votes` (
+  `UserID` int(11) NOT NULL,
+  `RoomID` int(11) NOT NULL,
+  `RestaurantID` int(11) NOT NULL,
+  `VoteTime` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `menuitems`
+--
+ALTER TABLE `menuitems`
+  ADD PRIMARY KEY (`ItemID`),
+  ADD KEY `RestaurantID` (`RestaurantID`);
+
+--
+-- Indexes for table `menupreferences`
+--
+ALTER TABLE `menupreferences`
+  ADD PRIMARY KEY (`UserID`,`ItemID`),
+  ADD KEY `ItemID` (`ItemID`);
+
+--
+-- Indexes for table `restaurants`
+--
+ALTER TABLE `restaurants`
+  ADD PRIMARY KEY (`RestaurantID`);
+
+--
+-- Indexes for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`ReviewID`),
+  ADD KEY `UserID` (`UserID`),
+  ADD KEY `RestaurantID` (`RestaurantID`);
+
+--
+-- Indexes for table `roommembers`
+--
+ALTER TABLE `roommembers`
+  ADD PRIMARY KEY (`RoomID`,`UserID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- Indexes for table `rooms`
+--
+ALTER TABLE `rooms`
+  ADD PRIMARY KEY (`RoomID`),
+  ADD UNIQUE KEY `RoomCode` (`RoomCode`),
+  ADD KEY `CreatedBy` (`CreatedBy`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`UserID`),
+  ADD UNIQUE KEY `Username` (`Username`),
+  ADD UNIQUE KEY `Email` (`Email`);
+
+--
+-- Indexes for table `votes`
+--
+ALTER TABLE `votes`
+  ADD PRIMARY KEY (`UserID`,`RoomID`,`RestaurantID`),
+  ADD KEY `RoomID` (`RoomID`),
+  ADD KEY `RestaurantID` (`RestaurantID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `menuitems`
+--
+ALTER TABLE `menuitems`
+  MODIFY `ItemID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `restaurants`
+--
+ALTER TABLE `restaurants`
+  MODIFY `RestaurantID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `ReviewID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rooms`
+--
+ALTER TABLE `rooms`
+  MODIFY `RoomID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `menuitems`
+--
+ALTER TABLE `menuitems`
+  ADD CONSTRAINT `menuitems_ibfk_1` FOREIGN KEY (`RestaurantID`) REFERENCES `restaurants` (`RestaurantID`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `menupreferences`
+--
+ALTER TABLE `menupreferences`
+  ADD CONSTRAINT `menupreferences_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `menupreferences_ibfk_2` FOREIGN KEY (`ItemID`) REFERENCES `menuitems` (`ItemID`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD CONSTRAINT `reviews_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `reviews_ibfk_2` FOREIGN KEY (`RestaurantID`) REFERENCES `restaurants` (`RestaurantID`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `roommembers`
+--
+ALTER TABLE `roommembers`
+  ADD CONSTRAINT `roommembers_ibfk_1` FOREIGN KEY (`RoomID`) REFERENCES `rooms` (`RoomID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `roommembers_ibfk_2` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `rooms`
+--
+ALTER TABLE `rooms`
+  ADD CONSTRAINT `rooms_ibfk_1` FOREIGN KEY (`CreatedBy`) REFERENCES `users` (`UserID`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `votes`
+--
+ALTER TABLE `votes`
+  ADD CONSTRAINT `votes_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `votes_ibfk_2` FOREIGN KEY (`RoomID`) REFERENCES `rooms` (`RoomID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `votes_ibfk_3` FOREIGN KEY (`RestaurantID`) REFERENCES `restaurants` (`RestaurantID`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
